f5a089350fef47963f7cf2d8c0febfbfa5407cc7
Steve Harmeyer <harmeyersteve@gmail.com>
2019-03-05 11:13:42 -0700

    updates

--- src/Providers/LaravelApiServiceProvider.php
+++ src/Providers/LaravelApiServiceProvider.php
@@ -7,10 +7,24 @@ use Submtd\LaravelApi\Services\Api;
 
 class LaravelApiServiceProvider extends ServiceProvider
 {
+    /**
+     * register method
+     */
     public function register()
     {
+        // bind api class to the service container
         $this->app->bind('api', function () {
             return new Api();
         });
+    }
+
+    /**
+     * boot method
+     */
+    public function boot()
+    {
+        // config files
+        $this->mergeConfigFrom(__DIR__ . '/../../config/laravel-api.php', 'laravel-api');
+        $this->publishes([__DIR__ . '/../../config' => config_path()], 'config');
     }
 }
--- src/Services/Api.php
+++ src/Services/Api.php
@@ -2,10 +2,132 @@
 
 namespace Submtd\LaravelApi\Services;
 
+use Illuminate\Support\Facades\Response;
+
 class Api
 {
-    public function test()
+    /**
+     * @var $data
+     * stores data that is returned to the client
+     */
+    protected $data = [];
+
+    /**
+     * @var $errors
+     * stores errors
+     */
+    protected $errors = [];
+
+    /**
+     * @var $status
+     * http status code
+     */
+    protected $status = 200;
+
+    /**
+     * set the data property
+     * @param array|object $data
+     * @return Api
+     */
+    public function setData($data)
     {
-        return 'hello';
+        if (is_array($data)) {
+            $this->data = $data;
+            return $this;
+        }
+        if (!method_exists($data, 'toArray')) {
+            throw new \Exception('$data must be an array or an object implementing the toArray method.', 400);
+        }
+        $this->data = $data->toArray();
+        return $this;
+    }
+
+    /**
+     * add a data value
+     * @param string $key - item key
+     * @param mixed $value - item value
+     * @return Api
+     */
+    public function addData(string $key, $value)
+    {
+        $this->data[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * get the data property
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * add an error to the errors property
+     * @param mixed $code
+     * @param string $description
+     * @return Api
+     */
+    public function addError($code, string $description)
+    {
+        $this->errors[] = [
+            'status' => (string) $code,
+            'title' => config('laravel-api.errorTitles.' . $code, 'Unknown error.'),
+            'detail' => $description,
+        ];
+        $this->setStatus($code);
+        return $this;
+    }
+
+    /**
+     * get the errors property
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * set the status code
+     * @param mixed $status
+     * @return Api
+     */
+    public function setStatus($status)
+    {
+        if (!is_numeric($status)) {
+            return $this;
+        }
+        if ($status < 100 || $status > 599) {
+            return $this;
+        }
+        $this->status = (int) $status;
+        return $this;
+    }
+
+    /**
+     * get the status code
+     * @return int
+     */
+    public function getStatus()
+    {
+        return (int) $this->status;
+    }
+
+    /**
+     * respond
+     * @return Response
+     */
+    public function respond()
+    {
+        if (!empty($this->getErrors())) {
+            return Response::json([
+                'errors' => $this->getErrors(),
+            ], $this->getStatus());
+        }
+        return Response::json([
+            'data' => $this->getData(),
+        ], $this->getStatus());
     }
 }
